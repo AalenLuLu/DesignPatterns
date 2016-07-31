88e3a89acd9b1e6f0e76f6e1ef7061c074c59997
LuLu <Aalen@AalendeiMac.local>
2016-08-01 00:45:37 +0800

    Interpreter

---
+++ Interpreter/Interpreter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Interpreter
+//
+//  Created by Aalen on 16/8/1.
+//  Copyright © 2016年 Aalen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
